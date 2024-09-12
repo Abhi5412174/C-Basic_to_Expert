7b50b7303b052c8c85a0dcbff136776bd535837c
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2024-09-13 00:15:58 +0545

    problem2_

--- Beginner/problem2.cpp
+++ Beginner/problem2.cpp
@@ -2,10 +2,25 @@
 // Operators for Fundamental Types
 
 #include <iostream>
-#include <iomanip>
 using namespace std;
 
 int main()
 {
-    
+    float num1, num2;
+
+    cout << "For Addition:";
+    cin >> num1 >> num2;
+    cout << "The sum is: " << num1 + num2;
+
+    cout << "\nFor Subtraction:";
+    cin >> num1 >> num2;
+    cout << "The sub is: " << num1 - num2;
+
+    cout << "\nFor multiplication:";
+    cin >> num1 >> num2;
+    cout << "The product is: " << num1 * num2;
+
+    cout << "\nFor division:";
+    cin >> num1 >> num2;
+    cout << "The divident is: " << num1 / num2;
 }
