db0a1824abc0a10ab8fd4fca871d84a11e73a9e1
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2024-09-13 20:22:33 +0545

    problem5_

---
+++ Beginner/problem5.cpp
@@ -0,0 +1,17 @@
+// concatination
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string name = "User1";
+    string name2 = "User2";
+    string name3;
+
+    name3 = name + name2;
+    cout << name3;
+
+    return 0;
+}
